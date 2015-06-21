c2092f8bf3884b76f7cb970ab297d742069832ae
Yunus Eren Guzel <exculuber@gmail.com>
2015-06-21 22:56:09 +0300

    logout is added

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,8 @@ Rails.application.routes.draw do
 
   post '/' => 'chat#register'
 
+  get 'logout' => 'chat#logout'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
