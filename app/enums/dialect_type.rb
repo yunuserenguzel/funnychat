95446c52e8135f300cff988bf1759962dbc3e30f
Yunus Eren Guzel <exculuber@gmail.com>
2015-06-21 17:17:15 +0300

    dialect type

--- app/enums/dialect_type.rb
+++ app/enums/dialect_type.rb
@@ -1,8 +1,7 @@
 class DialectType
-  def self.yoda; :yoda end
-  def self.pirate; :pirate end
-  def self.valley_girl; :valley_girl end
+
   @@types = [:yoda,:pirate,:valley_girl]
+
   def self.method_missing(m,*args, &block)
     if @@types.include?(m)
       return m
