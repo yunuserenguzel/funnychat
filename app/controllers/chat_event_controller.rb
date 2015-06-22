5d85b54180640d6766c06eba21cc67424914c003
Yunus Eren Guzel <exculuber@gmail.com>
2015-06-22 09:17:45 +0300

    removed unnecessary send messeage

--- app/controllers/chat_event_controller.rb
+++ app/controllers/chat_event_controller.rb
@@ -1,7 +1,8 @@
 class ChatEventController < WebsocketRails::BaseController
 
   def create_message
-    send_message :new_message, {text:message, dialect:session[:user_dialect], username:session[:username]}
+    # send_message :new_message, {text:message, dialect:session[:user_dialect], username:session[:username]}
+    WebsocketRails[:chat].trigger :new_message, {text:message, dialect:session[:user_dialect], username:session[:username]}
   end
 
 end
