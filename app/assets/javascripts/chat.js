53d7a1a08c1936a7a3ced1be742be30f5bee91ce
Yunus Eren Guzel <exculuber@gmail.com>
2015-06-22 10:22:20 +0300

    added styles for chat screen

--- app/assets/javascripts/chat.js
+++ app/assets/javascripts/chat.js
@@ -2,7 +2,7 @@ MAX_INT = 9007199254740992;
 function initializeChat(){
   $(document).ready(function () {
     Chat.startChat();
-    Chat.onMessageReceived(null);
+    $('#chat-input input').focus();
   });
 }
 
@@ -15,9 +15,12 @@ function keyPressed(event,input){
 function appendMessage(message){
   var messageElement = document.createElement('div');
   messageElement.className = 'chat-message';
-  $(messageElement).append('<img src="assets/images/'+message.dialect+'_icon.png">')
+  var userHolder = document.createElement('div');
+  userHolder.className = 'chat-message-user-holder';
+  $(userHolder).append('<img class="chat-message-icon" src="/images/'+message.dialect+'_icon.png">')
+  $(userHolder).append('<p class="chat-message-username">'+message.username+'</p>');
+  $(messageElement).append(userHolder);
   $(messageElement).append('<p class="chat-message-text">'+message.text+'</p>');
-  $(messageElement).append('<p class="chat-message-username">'+message.username+'</p>');
   $('#chat-area').append(messageElement);
   $("#chat-area").animate({ scrollTop: $('#chat-area').get(0).scrollHeight }, 500);
 }
@@ -28,6 +31,7 @@ var Chat = {
     Chat.dispatcher.on_open = Chat.onOpen;
     Chat.channel = Chat.dispatcher.subscribe('chat');
     Chat.channel.bind('new_message',Chat.onMessageReceived);
+    Chat.channel.bind('users_updated', Chat.onUserListUpdated())
   },
   sendMessage: function (text) {
     Chat.dispatcher.trigger('create_message',text);
@@ -38,6 +42,11 @@ var Chat = {
   },
   onOpen: function (data) {
     console.log(data);
+  },
+  onUserListUpdated: function (users) {
+    for(var i=0; i<users.length;i++) {
+
+    }
   }
 
 }
