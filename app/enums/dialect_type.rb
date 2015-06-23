0ba18c76e945d005a1f0931685dcf138f39b99af
Yunus Eren Guzel <exculuber@gmail.com>
2015-06-23 22:02:18 +0300

    dialect enum is changed

--- app/enums/dialect_type.rb
+++ app/enums/dialect_type.rb
@@ -1,17 +1,7 @@
-class DialectType
+module DialectType
 
-  @@types = [:yoda,:pirate,:valley_girl]
-
-  def self.method_missing(m,*args, &block)
-    if @@types.include?(m)
-      return m
-    else
-      throw "#{m} is not defined"
-    end
-  end
-
-  def self.all
-    @@types
-  end
+  Yoda = :yoda
+  Pirate = :pirate
+  ValleyGirl = :valley_girl
 
 end
