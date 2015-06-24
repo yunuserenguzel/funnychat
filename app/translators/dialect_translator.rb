1489009ba86c226edba25fb325d8419e49110890
Yunus Eren Guzel <exculuber@gmail.com>
2015-06-24 22:57:20 +0300

    dialect translator argument check and raise argument error

--- app/translators/dialect_translator.rb
+++ app/translators/dialect_translator.rb
@@ -7,7 +7,11 @@ class DialectTranslator
     }
   end
   def self.translate(dialect_type,text)
-    translator = translators_table[dialect_type].new(text)
+    translator_class = translators_table[dialect_type]
+    if !translator_class
+      raise ArgumentError, 'dialect_type must be a DialectType'
+    end
+    translator = translator_class.new(text)
     translator.translate
   end
 end
