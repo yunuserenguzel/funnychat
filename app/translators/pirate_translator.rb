6ba14bda8de89169e367d2acad93114ed922acb9
Yunus Eren Guzel <exculuber@gmail.com>
2015-06-25 23:07:27 +0300

    pirate translator is implemented

--- app/translators/pirate_translator.rb
+++ app/translators/pirate_translator.rb
@@ -1,2 +1,15 @@
 class PirateTranslator < DialectTranslatorBase
+  def initialize(text)
+    super text
+    self.host = 'http://isithackday.com/arrpi.php'
+  end
+  def request
+    RestClient.get host,{params:{text:text}} do |response, request, result|
+      return response
+    end
+  end
+  def parse(raw_text)
+    raw_text
+  end
+
 end
