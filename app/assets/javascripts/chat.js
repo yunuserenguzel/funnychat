f37547f1c75483a343a249cba15066e9516789a0
Yunus Eren Guzel <exculuber@gmail.com>
2015-06-29 17:32:07 +0300

    appended bell notification to chat area

--- app/assets/javascripts/chat.js
+++ app/assets/javascripts/chat.js
@@ -1,3 +1,5 @@
+document.title = 'Funny Chat';
+
 function initializeChat() {
   $(document).ready(function () {
     Chat.startChat();
@@ -38,20 +40,38 @@ function appendMessage(message) {
   $("#chat-area").animate({ scrollTop: $('#chat-area').get(0).scrollHeight }, 500);
 }
 
+function appendRingBell(data) {
+  var ringBellElement = document.createElement('div');
+  ringBellElement.className = 'ring-bell-element';
+  $(ringBellElement).append(data.name + ' has ringed you!');
+  $('#chat-area').append(ringBellElement);
+  $("#chat-area").animate({ scrollTop: $('#chat-area').get(0).scrollHeight }, 500);
+
+  document.title = 'Ring!';
+  setTimeout(function () {
+    document.title = 'Funny Chat';
+  },1000)
+}
+
 function setUsers(users) {
   $('#user-list').html('');
   for(var i=0; i<users.length;i++) {
     var user = users[i];
     var userElement = document.createElement('div');
     userElement.className = 'user-element';
-    $(userElement).append('<button onclick="ringUser(\''+user.id+'\')">Ring</button>');
+    $(userElement).append('<button onclick="ringUser(\''+user.id+'\',this)">Ring</button>');
     $(userElement).append('<strong>'+user.name+'</strong>');
     $('#user-list').append(userElement);
   }
 }
 
-function ringUser(userId) {
-  Chat.sendRingBell(userId);
+function ringUser(userId, button) {
+  button.disabled = true;
+  Chat.sendRingBell(userId, function () {
+    button.disabled = false;
+  }, function () {
+    button.disabled = true;
+  });
 }
 
 function logout() {
@@ -85,13 +105,14 @@ var Chat = {
   close: function () {
     Chat.dispatcher.disconnect();
   },
-  sendRingBell:function(userId) {
-    Chat.dispatcher.trigger('ring_bell',userId);
+  sendRingBell:function(userId,success,error) {
+    Chat.dispatcher.trigger('ring_bell',userId, success,error);
   },
   onRingBellReceived: function (data) {
     var audio = new Audio('/Desk-bell-sound.wav');
     console.log(data);
     audio.play();
+    appendRingBell(data);
   }
 
 }
